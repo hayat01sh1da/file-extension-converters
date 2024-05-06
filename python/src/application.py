b590e62b9b4c130c55cf6cf3a6dae068e589bcb7
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-05-06 18:45:09 +0900

    Commit application files

---
+++ python/src/application.py
@@ -0,0 +1,34 @@
+import os
+import glob
+import shutil
+
+class Application:
+    def __init__(self, original_extension, target_extension, mode = None):
+        self.target_files     = glob.glob(os.path.join('.', '**', '*{original_extension}'.format(original_extension = original_extension)), recursive = True)
+        self.target_extension = target_extension
+        self.mode             = mode
+
+    def run(self):
+        print('Target dirname is {current_directory}'.format(current_directory = os.getcwd()))
+        if len(self.target_files) > 1:
+            print('========== [{exec_mode}] Total File Extensions Count to Convert: {files_count} =========='.format(exec_mode = self.__exec_mode__(), files_count = len(self.target_files)))
+            print('========== [{exec_mode}] Start Converting File Extensions'.format(exec_mode = self.__exec_mode__()))
+            for target_file in self.target_files:
+                print('========== [{exec_mode}] Cleaning {target_file} =========='.format(exec_mode = self.__exec_mode__(), target_file = target_file))
+                if self.mode == '-e':
+                    shutil.move(target_file, self.__destination_file__(target_file))
+                print('========== [{exec_mode}] Converted File Extension: {target_file} => {destination_file} =========='.format(exec_mode = self.__exec_mode__(), target_file = target_file, destination_file = self.__destination_file__(target_file)))
+            print('========== [{exec_mode}] Total Converted File Extensions Count: {files_count} =========='.format(exec_mode = self.__exec_mode__(), files_count = len(self.target_files)))
+        else:
+            print('========== [{exec_mode}] No File with {original_extension} Remains =========='.format(exec_mode = self.__exec_mode__(), original_extension = self.original_extension))
+
+    # private
+
+    def __exec_mode__(self):
+        if self.mode == '-e':
+            return 'EXECUTION'
+        else:
+            return 'DRY_RUN'
+
+    def __destination_file__(self, target_file):
+        return '{dirname}/{basename}{target_extension}'.format(dirname = os.path.dirname(target_file), basename = os.path.splitext(os.path.basename(target_file))[0], target_extension = self.target_extension)
---
+++ ruby/src/application.rb
@@ -0,0 +1,40 @@
+require 'fileutils'
+
+class Application
+  def self.run(original_extension:, target_extension:, mode: nil)
+    self.new(original_extension, target_extension, mode).run
+  end
+
+  def initialize(original_extension, target_extension, mode)
+    @target_files     = Dir.glob(File.join('.', '**', "*#{original_extension}"))
+    @target_extension = target_extension
+    @mode             = mode
+  end
+
+  def run
+    puts "Current Directory is #{Dir.getwd}"
+    if !target_files.empty?
+      puts "========== [#{exec_mode}] Total File Extensions Count to Convert: #{target_files.size} =========="
+      puts "========== [#{exec_mode}] Start Converting File Extensions =========="
+      target_files.each { |target_file|
+        FileUtils.mv(target_file, destination_file(target_file)) if mode == '-e'
+        puts "========== [#{exec_mode}] Converted File Extension: #{target_file} => #{destination_file(target_file)} =========="
+      }
+      puts "========== [#{exec_mode}] Total Converted File Extensions Count: #{target_files.size} =========="
+    else
+      puts "========== [#{exec_mode}] No File with #{original_extension} Remains =========="
+    end
+  end
+
+  private
+
+  attr_reader :target_files, :target_extension, :mode
+
+  def exec_mode
+    mode == '-e' ? 'EXECUTION' : 'DRY RUN'
+  end
+
+  def destination_file(target_file)
+    "#{File.dirname(target_file)}/#{File.basename(target_file, '.*')}#{target_extension}"
+  end
+end
