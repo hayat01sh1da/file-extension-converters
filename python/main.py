df6ad6cf535acef3baf9befad78de55a321b734e
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-09-28 16:48:12 +0900

    Make user inputs interactive

--- python/main.py
+++ python/main.py
@@ -2,10 +2,8 @@ import sys
 sys.path.append('./src')
 from application import Application
 
-try:
-    _, original_extension, target_extension, mode, *_ = sys.argv
-except ValueError:
-    _, original_extension, target_extension, *_ = sys.argv
-    mode                                        = '-d'
+original_extension = input('Provide the original extension of the files you would like to change(e.g., .jpg, .png, .rb, .py, .ts. .js etc.): ')
+target_extension   = input('Provide the target extension of the files you would like to switch to by the change(e.g., .jpg, .png, .rb, .py, .ts. .js etc.): ')
+mode               = input('Provide -e(execution) if you would truly like to delete the files. This operation is cannot be undone, so trying to run without -e(dry_run) once is strongly recommended: ')
 
 Application(original_extension, target_extension, mode).run()
--- ruby/main.rb
+++ ruby/main.rb
@@ -1,4 +1,12 @@
 require_relative './src/application'
 
-original_extension, target_extension, mode, *_ = ARGV
+puts 'Provide the original extension of the files you would like to change(e.g., .jpg, .png, .rb, .py, .ts. .js etc.)'
+original_extension = gets.chomp
+
+puts 'Provide the target extension of the files you would like to switch to by the change(e.g., .jpg, .png, .rb, .py, .ts. .js etc.)'
+target_extension = gets.chomp
+
+puts 'Provide -e(execution) if you would truly like to delete the files. This operation is cannot be undone, so trying to run without -e(dry_run) once is strongly recommended'
+mode = gets.chomp
+
 Application.run(original_extension:, target_extension:, mode:)
