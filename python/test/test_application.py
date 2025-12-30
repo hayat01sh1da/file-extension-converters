b1e9fe96105d63a58e7ca64446405a423efa12ba
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-12-31 01:55:34 +0900

    Explicitly declare parameter names

--- python/test/test_application.py
+++ python/test/test_application.py
@@ -34,18 +34,19 @@ class TestApplication(unittest.TestCase):
         with self.assertRaises(InvalidModeError) as cm:
             Application(original_extension = self.original_extension,  target_extension = self.target_extension, mode = 'a').run()
         self.assertEqual('a is invalid mode. Provide either `d`(default) or `e`.', str(cm.exception))
+
     def test_run_in_dry_run_mode_1(self):
-      Application(self.original_extension, self.target_extension).run()
+      Application(original_extension = self.original_extension,  target_extension = self.target_extension ).run()
       self.assertEqual(len(glob.glob(os.path.join(self.dirname, '**', f'*{self.original_extension}'), recursive = True)), 100)
       self.assertEqual(len(glob.glob(os.path.join(self.dirname, '**', f'*{self.target_extension}'), recursive = True)), 0)
 
     def test_run_in_dry_run_mode_2(self):
-      Application(self.original_extension, self.target_extension, 'd').run()
+      Application(original_extension = self.original_extension,  target_extension = self.target_extension, mode = 'd').run()
       self.assertEqual(len(glob.glob(os.path.join(self.dirname, '**', f'*{self.original_extension}'), recursive = True)), 100)
       self.assertEqual(len(glob.glob(os.path.join(self.dirname, '**', f'*{self.target_extension}'), recursive = True)), 0)
 
     def test_run_in_exec_mode(self):
-      Application(self.original_extension, self.target_extension, 'e').run()
+      Application(original_extension = self.original_extension, target_extension = self.target_extension, mode = 'e').run()
       self.assertEqual(len(glob.glob(os.path.join(self.dirname, '**', f'*{self.original_extension}'), recursive = True)), 0)
       self.assertEqual(len(glob.glob(os.path.join(self.dirname, '**', f'*{self.target_extension}'), recursive = True)), 100)
 
